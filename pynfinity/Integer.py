aa8b6df82c9c391040c3f93283f4c7f33a5e53a4
extremeplay <ionescu.a.mihai@gmail.com>
2014-09-28 10:38:41 +0300

    Added Multiply, made small design improvements for test cases, improved doc

--- pynfinity/Integer.py
+++ pynfinity/Integer.py
@@ -8,9 +8,16 @@ class Integer:
             for i in reversed(range(len(num))):
                 self.storage.append(ord(num[i]) - ord("0"))
 
-    def __add__(self, other):
+    def __len__(self):
+        return len(self.storage)
 
-        """ Add two arbitrary-precision integer
+    def __getitem__(self, index):
+        return self.storage[index]
+
+    def __add__(self, other):
+        """ Add two Integer objects
+        :param other: Integer
+        :return: Integer storing the result of add
         """
         i = 0
         carry = 0
@@ -23,6 +30,35 @@ class Integer:
             i += 1
 
         return res
+
+    def __mul__(self, other):
+
+        """ Multiply two Integer objects
+        :param other: Integer
+        :return: Integer storing the result of multiply
+        """
+        res = Integer()
+        a = 0
+        for i in range(len(self)):
+
+            b = a
+            j = 0
+            t = 0
+            while j < len(other) or t > 0:
+
+                if b >= len(res):
+                    res.storage.append(0)
+                t += res[b] + (self[i] * other[j] if j < len(other) else 0)
+                res.storage[b] = t % self.BASE
+                b += 1
+                t /= 10
+                j += 1
+            a += 1
+
+        return res
+
+    def __eq__(self, other):
+        return self.storage == other.storage
 
     def __repr__(self):
 
--- test/TestInteger.py
+++ test/TestInteger.py
@@ -3,11 +3,15 @@ import unittest
 
 
 class TestInteger(unittest.TestCase):
-    def test_something(self):
-        a = pynfinity.Integer("1")
-        b = pynfinity.Integer("2")
-        c = a + b
-        self.assertEqual(str(c), "3")
+    def test_add(self):
+
+        self.assertEqual(pynfinity.Integer("1") + pynfinity.Integer("2"), pynfinity.Integer("3"))
+
+    def test_multiply(self):
+
+        self.assertEqual(pynfinity.Integer("30") * pynfinity.Integer("32"), pynfinity.Integer("960"))
+        self.assertEqual(pynfinity.Integer("15") * pynfinity.Integer("15"), pynfinity.Integer("225"))
+        self.assertEqual(pynfinity.Integer("25") * pynfinity.Integer("25"), pynfinity.Integer("625"))
 
 if __name__ == '__main__':
     unittest.main()
